c4e63b715aa6fe1d1c3782722607abfe26ce1f18
pghezzi <100241979+pghezzi@users.noreply.github.com>
2026-09-08 18:01:58 -0400

    Testing actual policy

--- controller.py
+++ controller.py
@@ -817,16 +817,16 @@ class DepthWaQController(TSController):
 
         # Build low cmd
         for i, motor_idx in enumerate(self.config.leg_joint2motor_idx):
-            self.low_cmd.motor_cmd[motor_idx].q = self.config.default_angles[i]
-            self.low_cmd.motor_cmd[motor_idx].dq = 0
-            self.low_cmd.motor_cmd[motor_idx].kp = self.config.stand_kp / 2
-            self.low_cmd.motor_cmd[motor_idx].kd = self.config.stand_kd / 2
-            
-            #self.low_cmd.motor_cmd[motor_idx].q = target_dof_pos[i]
+            #self.low_cmd.motor_cmd[motor_idx].q = self.config.default_angles[i]
             #self.low_cmd.motor_cmd[motor_idx].dq = 0
-            #self.low_cmd.motor_cmd[motor_idx].kp = self.config.ctrl_kp
-            #self.low_cmd.motor_cmd[motor_idx].kd = self.config.ctrl_kd
-            #self.low_cmd.motor_cmd[motor_idx].tau = 0
+            #self.low_cmd.motor_cmd[motor_idx].kp = self.config.stand_kp / 2
+            #self.low_cmd.motor_cmd[motor_idx].kd = self.config.stand_kd / 2
+            #
+            self.low_cmd.motor_cmd[motor_idx].q = target_dof_pos[i]
+            self.low_cmd.motor_cmd[motor_idx].dq = 0
+            self.low_cmd.motor_cmd[motor_idx].kp = self.config.ctrl_kp
+            self.low_cmd.motor_cmd[motor_idx].kd = self.config.ctrl_kd
+            self.low_cmd.motor_cmd[motor_idx].tau = 0
 
         self._record_timing(
             "inference",
