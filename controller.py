d379b603ab95232022988361d2d0b4e9e2decb80
pghezzi <100241979+pghezzi@users.noreply.github.com>
2026-09-08 17:22:09 -0400

    reduced kp kd due to falling

--- controller.py
+++ controller.py
@@ -787,8 +787,8 @@ class DepthWaQController(TSController):
         for i, motor_idx in enumerate(self.config.leg_joint2motor_idx):
             self.low_cmd.motor_cmd[motor_idx].q = self.config.default_angles[i]
             self.low_cmd.motor_cmd[motor_idx].dq = 0
-            self.low_cmd.motor_cmd[motor_idx].kp = self.config.stand_kp
-            self.low_cmd.motor_cmd[motor_idx].kd = self.config.stand_kd
+            self.low_cmd.motor_cmd[motor_idx].kp = self.config.stand_kp / 2
+            self.low_cmd.motor_cmd[motor_idx].kd = self.config.stand_kd / 2
             
             #self.low_cmd.motor_cmd[motor_idx].q = target_dof_pos[i]
             #self.low_cmd.motor_cmd[motor_idx].dq = 0
